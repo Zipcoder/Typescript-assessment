ae8d594f8ec37acd5e64e5f615e8a6060f1684f6
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-04-04 11:55:25 -0400

    Everything done

--- src/providers/wutang/wutang.spec.ts
+++ src/providers/wutang/wutang.spec.ts
@@ -1,16 +1,59 @@
 import { WutangProvider } from './wutang';
-
-let wuTang = null;
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing'
 
 describe('WuTang Service', () => {
 
+    let wuTang: WutangProvider;
+    let httpMock: HttpTestingController;
+
     beforeEach(() => {
-        wuTang = new WutangProvider();
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [WutangProvider]
+        });
+
+        wuTang = TestBed.get(WutangProvider);
+        httpMock = TestBed.get(HttpTestingController);
+
     });
 
-    it('should return children', () => {
-        let result = wuTang.wuTangIs();
-        expect(result).toBe('For the Children');
+    // it('should return children', () => {
+    //     let result = wuTang.wuTangIs();
+    //     expect(result).toBe('For the Children');
+    // });
+
+    it('should get data', () => {
+        wuTang.myFavoriteBand(1).subscribe((data: any) => {
+            expect(data.name).toBe('WuTang');
+        });
     });
-    
+
+    it('should know whats up', () => {
+        wuTang.myFavoriteBand(1).subscribe((data: any) => {
+            expect(data.name).toBe('WuTang');
+        });
+        const req = httpMock.expectOne(`http://www.mocky.io/v2/5185415ba171ea3a00704eed/1`, 'call to api');
+        expect(req.request.method).toBe('GET');
+        req.flush({
+            name: 'WuTang'
+        });
+        httpMock.verify();
+    });
+
+    it('should post the correct data', () => {
+        wuTang.newBandILike<any>('NWA').subscribe((data: any) => {
+          expect(data.firstname).toBe('NWA');
+        });
+      
+        const req = httpMock.expectOne(`http://www.mocky.io/v2/5185415ba171ea3a00704eed`, 'post to api');
+        expect(req.request.method).toBe('POST');
+      
+        req.flush({
+          firstname: 'NWA'
+        });
+      
+        httpMock.verify();
+      });
+
 });
--- src/providers/wutang/wutang.ts
+++ src/providers/wutang/wutang.ts
@@ -6,12 +6,20 @@ export class WutangProvider {
 
   whoWuTangIsFor: any;
 
-  constructor() {
+  constructor(private httpClient: HttpClient) {
     this.whoWuTangIsFor = "For the Children";
   }
 
   wuTangIs(){
     return this.whoWuTangIsFor;
+  }
+
+  myFavoriteBand<T>(id: number) {
+    return this.httpClient.get<T>(`http://www.mocky.io/v2/5185415ba171ea3a00704eed/${id}`)
+  }
+
+  newBandILike<T>(band: string) {
+    return this.httpClient.post<T>(`http://www.mocky.io/v2/5185415ba171ea3a00704eed/`, band)
   }
 
 }
