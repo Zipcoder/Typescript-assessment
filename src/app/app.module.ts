8fef7a4675a4ad70682c861bbb0638b92ae6538c
Haysel Santiago <hayselsantiago@zipcoders-MacBook-Pro-67.local>
2018-04-04 13:44:29 -0400

    Refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { DataProvider } from '../providers/data/data';
 
 @NgModule({
   declarations: [
@@ -26,6 +26,7 @@ import { HomePage } from '../pages/home/home';
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
+    DataProvider
   ]
 })
 export class AppModule {}
