72103ca3afec681b7eb806d6ed634790da6c9185
Carolynn Vansant <cvansant@MacBook-Pro-7.local>
2018-04-04 12:21:11 -0400

    part 2 working

--- src/providers/prove/prove.spec.ts
+++ src/providers/prove/prove.spec.ts
@@ -5,45 +5,41 @@ import {ProveProvider} from './prove';
 import { HttpClient } from '@angular/common/http';
 
 let children = null;
+let service: ProveProvider;
+let httpMock: HttpTestingController;
+
 
 describe('Return the Children', ()=>{
     
-    beforeEach(()=>{
-        
-    })
-
     it('should return a string saying For the Children', ()=>{
         let client = null;
         children = new ProveProvider(client);
         let result = children.wuTangIs();
         let expectation = "For the Children";
+
         expect(result).toBe(expectation);
     });
-    
+
 });
 
-// Didn't have quite enough time to get this one to pass
 
-// let service: ProveProvider;
-// let httpMock: HttpTestingController;
+describe('Get favorite band service', ()=>{
+    beforeEach(()=>{
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers:[ProveProvider]
+        });
+        service = TestBed.get(ProveProvider);
+        httpMock = TestBed.get(HttpTestingController);
+    });
 
-// describe('Get favorite band service', ()=>{
-//     beforeEach(()=>{
-//         TestBed.configureTestingModule({
-//             imports: [HttpClientTestingModule],
-//             providers:[ProveProvider]
-//         });
-
-//         it('should return a string for a band name', ()=>{
-//             service.myFavoriteBand(1).subscribe((data:any)=>{
-//                 expect(data.name).toBe('Creed');
-//             });
-//             const req = httpMock.expectOne(`localhost/8080/1`,'call to api');
-//             expect(req.request.method).toBe('GET');
-//         });
+        it('should return a string for a band name', ()=>{
+            service.myFavoriteBand(1).subscribe((data:any)=>{
+                expect(data.name).toBe('Creed');
+            });
+            const req = httpMock.expectOne(`localhost/8080/1`,'call to api');
+            expect(req.request.method).toBe('GET');
+        });
         
-//     });
-// })
-
-
+    });
 
