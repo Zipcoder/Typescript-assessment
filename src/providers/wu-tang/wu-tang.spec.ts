afc1c8f1d1e774a2a3a47bff396bf22cbe60c306
ahsonali <ahsonac@gmail.com>
2018-04-04 11:57:46 -0400

    done

---
+++ src/providers/wu-tang/wu-tang.spec.ts
@@ -0,0 +1,21 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+
+import {WuTangProvider} from './wu-tang';
+
+let service : WuTangProvider;
+let httpMock: HttpTestingController;
+
+beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [WuTangProvider]
+    });
+
+    service = TestBed.get(WuTangProvider);
+    httpMock = TestBed.get(HttpTestingController);
+});
+    it('Be for the kids', () => {
+        let result = service.wuTang()
+        expect(result).toContain('For the children');
+    });
---
+++ src/providers/wu-tang/wu-tang.ts
@@ -0,0 +1,22 @@
+import { HttpClient, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the WuTangProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class WuTangProvider {
+
+  constructor(public http: HttpClient) {
+    console.log('Hello WuTangProvider Provider');
+  }
+
+  wuTang() : string
+  {
+    return "For the Children";
+  }
+
+}
