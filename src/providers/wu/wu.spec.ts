af73ff6a385f84548ea6ac00a970b531ec8e5026
Kthomas <kthomas@github.com>
2018-04-04 12:53:47 -0400

    first test Wuuuuuuuu

--- src/providers/wu/wu.spec.ts
+++ src/providers/wu/wu.spec.ts
@@ -1,1 +1,14 @@
-import { wuProvider } from "./wu"
+import { WuProvider } from "./wu";
+
+let MethodMan = null;
+let HttpClient = null;
+
+    describe("Test For the Children", () => {
+        beforeEach(() => {
+            MethodMan = new WuProvider(HttpClient);
+        });
+        it("Should say For the Children", () => {
+            let output = MethodMan.wuTangIs();
+            expect(output).toBe("For the Children");
+        });
+    });
